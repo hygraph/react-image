ac7ba374e545a64915ed8e08d64da54a94f74bb7
Abhi Agarwal <abhi.agarwal@burrow.com>
2020-02-19 13:38:07 -0500

    Adding tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,20 +5,24 @@ describe('Utils tests', () => {
   const timeout = 3000;
 
   test('should get the correct widths', async () => {
-    const widths = new getWidths(2440, 4880);
+    const widths = getWidths(2440, 4880);
     expect(widths.length).to.equal(2);
     expect(widths[0]).to.equal(1220);
   }, timeout);
 
   test('should construct correct srcSet', async () => {
     const constructedURL = constructURL('test', false, 'https://burrow.com');
-    const widths = new getWidths(2440, 4880);
-    const srcSetImgs = new srcSet(constructedURL, widths, '', '');
-    console.log(srcSetImgs);
+    const widths = getWidths(1390, 800);
+    const srcSetImgs = srcSet(constructedURL, widths, 'crop', []);
+    expect(srcSetImgs).to.equal('https://burrow.com/resize=w:200,fit:crop/compress/test 200w,\n' +
+    'https://burrow.com/resize=w:400,fit:crop/compress/test 400w,\n' +
+    'https://burrow.com/resize=w:800,fit:crop/compress/test 800w,\n' +
+    'https://burrow.com/resize=w:1200,fit:crop/compress/test 1200w,\n' +
+    'https://burrow.com/resize=w:1390,fit:crop/compress/test 1390w');
   }, timeout);
 
   test('should get correct imgSizes', async () => {
-    const constructedURL = imgSizes(4480);
-    console.log(constructedURL);
+    const allImgSizes = imgSizes(4480);
+    expect(allImgSizes).to.equal('(max-width: 4480px) 100vw, 4480px');
   }, timeout);
 });
